64c00318414b0419b05ec7503b9b9a344fdb5741
vartolo007 <abdalrahmanalazhari2006@gmail.com>
2025-12-15 18:29:30 +0300

    'test' this is for mohamed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
         });
 
 
+
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
